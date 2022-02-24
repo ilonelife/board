098fafe61638855f804393c6a2d79587ce038c6d
iloneslife <jasony1@naver.com>
2022-02-24 12:17:24 +0900

    delete

---
+++ html/board/delete.php
@@ -0,0 +1,25 @@
+<?php
+
+include "../db.php";
+
+ // echo $_GET["_id"];
+
+$id = $_GET["_id"];
+
+$sql = "
+DELETE FROM board
+WHERE _id = ".$id."
+";
+
+
+$conn->query($sql);
+
+$conn->close();
+
+?>
+
+<script>
+
+    location.href="list.php";
+
+</script>
